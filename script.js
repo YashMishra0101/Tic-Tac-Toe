896810469c9744bb9a87be0df1ba5b17981d5099
Yash Mishra <yashrkmfedu6927@gmail.com>
2023-04-18 17:55:39 +0530

    small changes in js code

--- script.js
+++ script.js
@@ -112,6 +112,7 @@ const handelClick=(index)=>{
         box.addEventListener('click',()=>{
             handelClick(index); 
             playtik();
+            newGameBtn.classList.add("active");
             
         }) 
     })
