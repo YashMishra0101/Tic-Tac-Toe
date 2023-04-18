cdb61172b14d26961b59a089d21526fabc627f69
Yash Mishra <yashrkmfedu6927@gmail.com>
2023-04-18 14:42:59 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,40 @@
+<img src="./assects/Screenshot (257).png">
+
+<img src="./assects/Screenshot (258).png">
+
+# Tic Tac Toe Game
+
+This is a Tic Tac Toe game created as a practice project using HTML, CSS, and JavaScript. The game also includes animations and background audio for an enhanced user experience.
+
+## Game Features
+
+- Classic Tic Tac Toe game with a 3x3 game board.
+- Two players can play against each other on the same device.
+- Interactive animations for a visually appealing experience.
+- Reset option to restart the game at any time.
+
+## Technologies Used
+
+- HTML5: Used for creating the game board and the user interface.
+- CSS3: Used for styling the game board, animations, and user interface.
+- JavaScript: Used for implementing the game logic and interactions.
+
+## Animation and Audio
+
+- The game includes interactive animations to enhance the visual experience, including a 30-degree rotation of boxes when clicked.
+- The game includes animations for highlighting the winning cells and displaying the game result.
+- Audio is played when a player wins the game, and also when a player clicks on a game cell to make a move.
+
+
+## Contributing
+
+If you would like to contribute to this Tic-Tac-Toe project, please open an issue or submit a pull request. All contributions are welcome!
+
+
+## Feedback
+
+If you have any questions or suggestions, please feel free to contact me on LinkedIn 
+https://www.linkedin.com/in/yash-mishra-356280223
+
+
+
---
+++ script.js
@@ -0,0 +1,128 @@
+const gameInfo=document.querySelector('.game-info');
+const boxes=document.querySelectorAll('.box');
+const newGameBtn=document.querySelector('.btn');
+const clickMusic=document.querySelector('.gameOver')
+const tik=document.querySelector('.tik')
+
+let currentPlayer;
+let gameGrid;
+
+
+const initGame=()=>{
+    currentPlayer='🧛🏼';
+    gameGrid=["","","","","","","","",""];
+    boxes.forEach((box) => {
+        box.innerText = ""; 
+        box.classList.remove('win')
+        box.style.pointerEvents ='auto';
+    });
+    pauseAudio()
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText=`Current Player - ${currentPlayer}`
+}
+
+initGame();
+
+
+const  winningPositions=[ 
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+const swapTurn=()=>{
+    if(currentPlayer==="🧛🏼"){
+        currentPlayer=" 👹"
+    }
+    else{
+        currentPlayer="🧛🏼"
+    }
+    gameInfo.innerText=`Current Player - ${currentPlayer}`
+}
+
+function checkGameover(){
+    let answer="";
+
+    winningPositions.forEach((position)=>{
+
+     if((gameGrid[position[0]]!=="" || gameGrid[position[1]]!=="" || gameGrid[position[01]]!=="" ) &&
+        
+     (gameGrid[position[0]]===gameGrid[position[1]])  &&  (gameGrid[position[1]]===gameGrid[position[2]] )) {
+
+        if(gameGrid[position[0]]==='🧛🏼')
+
+            answer='🧛🏼';
+        
+             
+             else{
+            answer='👹';
+             }
+
+             boxes.forEach((box) => {
+                box.style.pointerEvents ='none';
+            })
+
+            boxes[position[0]].classList.add('win')
+            boxes[position[1]].classList.add('win')
+            boxes[position[2]].classList.add('win')
+            playAudio();
+          }
+
+    });
+
+    if(answer !== "" ) {
+        gameInfo.innerText = `Winner Player - ${answer}`;
+        newGameBtn.classList.add("active");
+    
+    }
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if(box !== "" )
+            fillCount++;
+    });
+
+    //board is Filled, game is TIE
+    if(fillCount === 9) {
+        gameInfo.innerText = "Game Tied !";
+        newGameBtn.classList.add("active");
+    }
+}
+
+
+
+const handelClick=(index)=>{
+    if(gameGrid[index]===""){
+        boxes[index].innerText=currentPlayer;
+        gameGrid[index]=currentPlayer;
+        swapTurn();
+        checkGameover();
+    }
+    }
+    
+    boxes.forEach((box,index)=>{
+        box.addEventListener('click',()=>{
+            handelClick(index); 
+            playtik();
+        }) 
+    })
+
+   
+newGameBtn.addEventListener('click',initGame)
+
+function playAudio() { 
+    clickMusic.play(); 
+  } 
+
+  function pauseAudio() {
+    clickMusic.pause();
+  }
+
+  
+function playtik() { 
+    tik.play(); 
+  } 
